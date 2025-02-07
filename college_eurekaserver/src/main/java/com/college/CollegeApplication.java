3dffd6320c0b875e6ebb59661db267ba5e497a81
Vishal9662 <104097927+Vishal9662@users.noreply.github.com>
2025-02-08 00:50:12 +0530

    added eureka server

---
+++ college_eurekaserver/src/main/java/com/college/CollegeApplication.java
@@ -0,0 +1,15 @@
+package com.college;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeApplication.class, args);
+	}
+
+}
---
+++ college_eurekaserver/src/test/java/com/college/CollegeApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.college;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
